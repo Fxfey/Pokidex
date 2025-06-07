69869ed1774ad7b3cac489febfc49f7a76501894
Ben <benjamin.w.fyfe@gmail.com>
2025-06-07 18:58:12 +0100

    fix: add background to title

--- src/app/components/ProfileTitle/ProfileTitle.styled.tsx
+++ src/app/components/ProfileTitle/ProfileTitle.styled.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 export const ProfileTitleStyled = styled.h1`
     font-size: 36px;
+    width: fit-content;
+    margin: auto;
+    padding: 0 15px;
+    border-radius: 0 0 8px 8px;
     font-weight: bold;
     text-align: center;
+    background-color: var(--black-1);
 `;
